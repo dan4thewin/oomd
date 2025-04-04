cc6125cb18dc5bbedb2c9b45dc995be64398ef79
Dan Good <dangood@microsoft.com>
2025-04-04 00:05:46 +0000

    Add simple plugins: Sleep, AlwaysReclaim

---
+++ src/oomd/plugins/Sleep.cpp
@@ -0,0 +1,50 @@
+#include "oomd/plugins/Sleep.h"
+
+#include "oomd/Log.h"
+#include "oomd/PluginRegistry.h"
+
+namespace Oomd {
+
+REGISTER_PLUGIN(sleep, Sleep::create);
+
+int Sleep::init(
+    const Engine::PluginArgs& args,
+    const PluginConstructionContext& context) {
+  using std::chrono::steady_clock;
+
+  argParser_.addArgument("duration", duration_, true);
+  if (!argParser_.parse(args)) {
+    return 1;
+  }
+
+  start_ = steady_clock::now();
+
+  // Success
+  return 0;
+}
+
+Engine::PluginRet Sleep::run(OomdContext& ctx) {
+  using std::chrono::steady_clock;
+
+  auto ret = Engine::PluginRet::STOP;
+  const auto now = steady_clock::now();
+  const auto diff =
+      std::chrono::duration_cast<std::chrono::seconds>(now - start_)
+          .count();
+
+  if (diff >= duration_) {
+    start_ = steady_clock::now();
+    ret = Engine::PluginRet::CONTINUE;
+  }
+
+#ifdef DEBUG
+  std::ostringstream oss;
+  oss << "duration=" << duration_ << " diff=" << diff
+      << " ret=" << (int) ret;
+  OLOG << oss.str();
+#endif
+
+  return ret;
+}
+
+} // namespace Oomd
